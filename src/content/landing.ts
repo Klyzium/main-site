6a9006dc96101db122e236066b7dcdd087ae983b
NewFabio <fabio.conceicaoads@gmail.com>
2026-04-15 17:27:11 -0300

    a

--- src/content/landing.ts
+++ src/content/landing.ts
@@ -132,7 +132,7 @@ export const CLIENT_LOGOS: ClientLogo[] = [
   {
     name: "Cliente 05",
     logo: "/logoClientes/jph.png",
-    url: "jornaldaprimeirahora.com.br",
+    url: "https://jornaldaprimeirahora.com.br",
   },
   {
     name: "Cliente 06",
